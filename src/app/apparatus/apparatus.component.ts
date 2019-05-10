96e23efbf334323d4fc5447547a72570f7b20489
NSANZABANDI Fiacre <fiacre.nsanzabandi@soprasteria.com>
2019-05-10 08:25:16 +0200

    Printed apparatus and their status

--- src/app/apparatus/apparatus.component.ts
+++ src/app/apparatus/apparatus.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApparatusComponent implements OnInit {
 
+  apparatusName: string = 'Machine à laver';
+  apparatusStatus: string = 'éteint';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getStatus() {
+    return this.apparatusStatus;
   }
 
 }
