85e71375754aecc285df3b956b7f6866d277788b
NSANZABANDI Fiacre <fiacre.nsanzabandi@soprasteria.com>
2019-05-17 09:25:01 +0200

    Corrected a bug on the device print.

--- src/app/device-edition/device-edition.component.ts
+++ src/app/device-edition/device-edition.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class DeviceEditionComponent implements OnInit {
 
-  defaultStatus = "switchedOff";
+  defaultStatus = "éteint";
 
   constructor(private deviceService: DeviceService,
               private router: Router ) { }
